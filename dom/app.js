2580bca77d94ee2445a2ad97e8058d2198e7dc15
Tanush Reddy <atanushreddy@gmail.com>
2024-02-26 12:49:18 +0530

    ,,

--- dom/app.js
+++ dom/app.js
@@ -3,3 +3,21 @@ const allImages=document.getElementsByTagName('img');
 for (let img of allimages){
     console.log(img.src)
 } 
+
+
+
+
+document.querySelector("#banner").id="whoops"
+document.querySelector("#whoops").id="banner"
+document.querySelector("a").href
+document.querySelector("a").title
+
+let x=document.querySelector("a")
+x.getAttribute('href')
+x.getAttribute('href',"www.google.com")
+
+document.querySelectorAll("a")[1] 
+document.querySelectorAll('input[type="text"]')
+
+
+document.getElementsByClassName
