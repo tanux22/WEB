a324f9737912c9563a5292e5614b0a042f67246e
Tanush Reddy <atanushreddy@gmail.com>
2024-02-27 10:14:47 +0530

    fv

--- dom/app.js
+++ dom/app.js
@@ -16,8 +16,31 @@ let x=document.querySelector("a")
 x.getAttribute('href')
 x.getAttribute('href',"www.google.com")
 
-document.querySelectorAll("a")[1] 
+document.querySelectorAll("a")[1]   
 document.querySelectorAll('input[type="text"]')
 
 
-document.getElementsByClassName
+let m=document.querySelector('a').title
+m.setAttribute(title,"hello")
+
+document.querySelector("input[type='text']")
+
+
+
+const l=document.querySelector("h1")
+window.getComputedStyle(l).color
+
+
+const allink=document.querySelectorAll('a');
+for (let link of allink){
+    link.style.textDecorationStyle="wavy"
+} 
+
+const h1=document.querySelector("h1")
+const sl=document.getAttribute("class")
+h1.getAttribute('class',`${s1} purple`)
+
+
+const ig=document.createElement("img")
+ig.src="C:\Users\Ganesh A\OneDrive\Pictures\Saved Pictures\IMG-20230528-WA0087.jpg"
+document.body.appendChild(ig)
