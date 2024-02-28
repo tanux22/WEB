2ab1803da4e8652995c15634423848042dc4c9bf
Tanush Reddy <atanushreddy@gmail.com>
2024-02-28 18:18:14 +0530

    fdf

--- events/app.js
+++ events/app.js
@@ -18,3 +18,35 @@ y.classList.add("lol");
 y.addEventListener('click',function(){
     console.log("good bye");
 })
+
+
+
+
+const butts=document.querySelectorAll("button");
+for (let but of butts){
+    but.addEventListener("click",colourize  )
+}
+
+
+const h1s=document.querySelectorAll("h1");
+for (let h of h1s)
+{
+    h.addEventListener("click",()=>{
+        h.style.backgroundColor=colormaker();
+        h.style.color=colormaker();
+    })
+}
+
+
+const colormaker= ()=>{
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    return `rgb(${r},${g},${b})`;
+}
+
+
+function colourize(){
+    this.style.backgroundColor=colormaker();
+    this.style.color=colormaker();
+}
